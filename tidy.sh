47a0db1a759ef02f611f8ea1fb3383549a7dca44
chrisahart <chrissahart@gmail.com>
2023-05-26 18:04:32 +0100

    Commented out printing

---
+++ tidy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#files=("qs_scf_methods.F")
+#files=("qs_charge_mixing.F" "qs_gspace_mixing.F" "qs_mixing_utils.F" "qs_outer_scf.F" "qs_scf.F" "qs_scf_diagonalization.F" "qs_scf_loop_utils.F" "qs_scf_methods.F" "qs_scf_output.F")
+
+#input_raw="WRITE"
+#input=$(printf '%s\n' "$input_raw" | sed -e 's/[\/&]/\\&/g')
+#echo $input
+
+#output_raw="WRITETEST"
+#output_raw="!WRITE (*, *)"
+#output=$(printf '%s\n' "$output_raw" | sed -e 's/[\/&]/\\&/g')
+#echo $output
+
+sed -i -e 's/"WRITE"/"WRITETEST"/g' src/qs_scf_methods.F
+
+#for file in "${files[@]}"; do
+#	echo $file
+#	sed -i -e 's/$input/$output/g' src/$file
+#done
