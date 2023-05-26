72f978de6c26286f5dc598ffa45c5734120e86af
chrisahart <chrissahart@gmail.com>
2023-05-26 19:02:11 +0100

    Cleaning code

--- tidy.sh
+++ tidy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 files=("qs_scf_initialization.F")
-#files=("qs_scf_initialization.F" "qs_charge_mixing.F" "qs_gspace_mixing.F" "qs_mixing_utils.F" "qs_outer_scf.F" "qs_scf.F" "qs_scf_diagonalization.F" "qs_scf_loop_utils.F" "qs_scf_methods.F" "qs_scf_output.F")
+#files=( "qs_scf_initialization.F" "qs_charge_mixing.F" "qs_gspace_mixing.F" "qs_mixing_utils.F" "qs_outer_scf.F" "qs_scf.F" "qs_scf_diagonalization.F" "qs_scf_loop_utils.F" "qs_scf_methods.F" "qs_scf_output.F")
 
 input_raw="WRITE (*, *) "
 input=$(printf '%s\n' "$input_raw" | sed -e 's/[\/&]/\\&/g')
